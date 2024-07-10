5a6e42bab97968c08abed1268fa32c20fdc6dcea
Jose Jordan <66675542+mundodigitalpro@users.noreply.github.com>
2024-07-10 20:38:51 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const message = document.getElementById('message');
+    setTimeout(() => {
+        message.style.opacity = 1;
+    }, 500);
+});
