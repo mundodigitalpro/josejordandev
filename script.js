79a3f7b4fa8dba26c61abe9b2c31ddb8891fc79a
Jose Jordan <66675542+mundodigitalpro@users.noreply.github.com>
2024-07-10 20:42:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const message = document.getElementById('message');
-    setTimeout(() => {
-        message.style.opacity = 1;
-    }, 500);
+    const text = "Hello World, I'm Jose Jordan";
+    let i = 0;
+
+    function typeWriter() {
+        if (i < text.length) {
+            message.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typeWriter, 100);
+        }
+    }
+
+    setTimeout(typeWriter, 1000);
 });
